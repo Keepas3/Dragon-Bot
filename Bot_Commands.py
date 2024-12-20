30f39995a17e659827a55f3dbed22752ee0e2ef6
Keepas3 <bfun679@gmail.com>
2024-12-19 22:20:55 -0500

    leaks command works for the most part +stats

--- Bot_Commands.py
+++ Bot_Commands.py
@@ -1,11 +1,16 @@
 import discord
 import os
 import logging
+import praw
 from discord.ext import commands
 import logging.handlers
 
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+client_id = os.getenv('client_id')
+client_secret = os.getenv('client_secret')
+user_agent = os.getenv('user_agent')
+
 
 class LevelFilter(logging.Filter):
     def __init__(self, level):
@@ -69,6 +74,42 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}!')
+
+@bot.command(name = 'stats')
+async def stats(ctx):
+    server_count =len(bot.guilds)
+    user_count = len(bot.users)
+    stats_message = (
+        f"**Bot Statistics**\n" 
+        f"Servers: {server_count}\n" 
+        f"Users: {user_count}\n"
+    )
+    await ctx.send(stats_message)
+
+reddit = praw.Reddit(client_id=client_id,client_secret=client_secret, user_agent=user_agent)
+
+@bot.command(name = 'leaks')
+async def leaks(ctx, subreddit_name: str, post_type: str ='hot', limit: int=3 ):
+    subreddit = reddit.subreddit(subreddit_name) 
+    
+    if post_type.lower() == 'new':
+        posts = subreddit.new(limit =10 )
+    elif post_type.lower() == 'top':
+        posts = subreddit.top(limit =10 )
+    else:
+        posts =subreddit.hot(limit =10 )
+    non_pinned_posts = [post for post in posts if not post.stickied][:limit]
+    if len(non_pinned_posts) < limit:
+        response = f"**Only {len(non_pinned_posts)} {post_type.capitalize()} Posts in r/{subreddit_name} available:**\n"
+    else: 
+        response = f"**Top {limit} {post_type.capitalize()} Posts in r/{subreddit_name}:**\n"
+    await ctx.send(response) 
+    for post in non_pinned_posts: 
+        embed = discord.Embed(title=post.title, url=post.url) 
+        if post.thumbnail and post.thumbnail.startswith("http"): 
+            embed.set_image(url=post.thumbnail) 
+            await ctx.send(embed=embed)
+
 
 @bot.event
 async def on_message(message):
